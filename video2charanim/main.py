acc3a5ff812f7cd27b0ac454a9ed2d0eefd78fee
yb123speed <904896209@qq.com>
2019-01-03 14:16:06 +0800

    img2chars

--- video2charanim/main.py
+++ video2charanim/main.py
@@ -31,3 +31,33 @@ def video2imgs(video_name, size):
     # 结束是要释放空间
     cap.release()
     return img_list
+
+pixels = " .,-'`:!1+*abcdefghijklmnopqrstuvwxyz<>()\/{}[]?234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ%&@#$"
+
+def img2chars(img):
+    res = []
+    """
+
+    :param img: numpy.ndarray,图像矩阵
+    :return: 字符串的列表：图像对应的字符画，其每一行对应图像的一行像素
+    """
+    
+    # 要注意这里的顺序和 之前的 size 刚好相反
+    height, width = img.shape
+    for row in range(height):
+        line = ""
+        for col  in range(width):
+            # 灰度是用8位表示的，最大值为255.
+            # 这里将灰度转化到0~1之间
+            percent = img[row][col] / 255
+
+            # 将灰度值进一步转换成 0 到 (len(pixels) - 1)之间，这样就好pixels里的字符对应起来了
+            index = int(percent * (len(pixels) - 1))
+
+            # 添加字符像素 （最后面加一个空格，是因为命令行有行距却几乎没有字符间距，用空格当间距）
+            line += pixels[index] + ' '
+        res.append(line)
+    return res
+
+# if __name__ == "__main__":
+#     video2imgs(r"I:\pygadgets\video2charanim\BadApple.mp4",(360,270))
