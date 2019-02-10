c5a6a6b1ec665558ccda0b591d7ffc4fd2dbb42c
yb123speed <904896209@qq.com>
2019-02-10 12:29:08 +0800

    监听键盘事件

--- aircraftwar/game.py
+++ aircraftwar/game.py
@@ -31,7 +31,18 @@ while True:
 
     # 绘制飞机
     screen.blit(player, player_pos)
-    
+
+    # 监听键盘事件
+    key_pressed = pygame.key.get_pressed()
+    if key_pressed[K_UP]:
+        player_pos[1] -= 3
+    if key_pressed[K_DOWN]:
+        player_pos[1] += 3
+    if key_pressed[K_LEFT]:
+        player_pos[0] -= 3
+    if key_pressed[K_RIGHT]:
+        player_pos[0] += 3
+
     # 更新屏幕
     pygame.display.update()
     # 处理游戏退出
