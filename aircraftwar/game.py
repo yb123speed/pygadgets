fdb2e1aba4c708ae2d0e09af408a9206ab42e0cc
yb123speed <904896209@qq.com>
2019-02-11 12:45:15 +0800

    敌人类

--- aircraftwar/game.py
+++ aircraftwar/game.py
@@ -106,4 +106,17 @@ class Player(pygame.sprite.Sprite):
         else:
             self.rect.left += self.speed
   
+
+# 敌人类
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, enemy_img, enemy_down_imgs, init_pos):
+       pygame.sprite.Sprite.__init__(self)
+       self.image = enemy_img
+       self.rect = self.image.get_rect()
+       self.rect.topleft = init_pos
+       self.down_imgs = enemy_down_imgs
+       self.speed = 2
+       self.down_index = 0
   
+    def move(self):
+        self.rect.top += self.speed
