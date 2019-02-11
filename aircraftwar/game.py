e66ca032b95dec708b786e3dee0e066a328d9173
yb123speed <904896209@qq.com>
2019-02-11 12:44:28 +0800

    子弹类

--- aircraftwar/game.py
+++ aircraftwar/game.py
@@ -50,3 +50,16 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+# 子弹类
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, bullet_img, init_pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = bullet_img
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = init_pos
+        self.speed = 10
+  
+    def move(self):
+        self.rect.top -= self.speed
+  
